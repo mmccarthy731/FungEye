f2d6ac2d51a242ca243874c3e5f91d00987cae2c
David Schwartz <david.schwartzy@gmail.com>
2017-12-05 09:09:44 -0500

    first push using cloudinary

--- FungeyeApp/Controllers/HomeController.cs
+++ FungeyeApp/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace FungeyeApp.Controllers
 {
     public class HomeController : Controller
     {
+        string APIKey = ConfigurationManager.AppSettings.Get("APIKey");
+        string APISecret = ConfigurationManager.AppSettings.Get("APISecret");
+        string ServerName = "fungeye";
         public ActionResult Index()
         {
             return View();
@@ -22,9 +28,20 @@ namespace FungeyeApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            Account account = new Account(ServerName, APIKey, APISecret); //hide API info
+            Cloudinary cloudinary = new Cloudinary(account);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(@"http://www.dole.com/~/media/Mastheads/All%20Fruit%20Final.jpg")//set string to input variable of user uploaded image (works with both URLs and file paths)
+
+            };
+            var uploadResult = cloudinary.Upload(uploadParams);
+
 
             return View();
         }
+
+        
     }
 }
